f25173439bacc140bd0528dc415ba0d78912f117
Jonathan Biro <jonnybrx@gmail.com>
2018-11-18 22:08:48 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,8 +3,17 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
   data.each do |pigeon_cat,pigeon_sub_cat|
-    pigeon_sub_cat.each do |prop, name|
-      binding.pry
+    pigeon_sub_cat.each do |prop, names|
+      names.each do |name|
+        if !pigeon_list[name]
+          pigeon_list[name] = {}
+
+        end
+        if !pigeon_list[name][pigeon_cat]
+          pigeon_list[name][pigeon_cat] = []
+        end
+        pigeon_list[name][pigeon_cat] << prop
+      end
 
     end
   end
