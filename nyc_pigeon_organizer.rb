10d52ea979debb84d9ca8ff4945931d06592586b
Jonathan Biro <jonnybrx@gmail.com>
2018-11-18 22:18:08 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |pigeon_cat,pigeon_sub_cat|
+    pigeon_sub_cat.each do |prop, names|
+      names.each do |name|
+        if !pigeon_list[name]
+          pigeon_list[name] = {}
+
+        end
+        if !pigeon_list[name][pigeon_cat]
+          pigeon_list[name][pigeon_cat] = []
+        end
+        pigeon_list[name][pigeon_cat] << prop.to_s
+      end
+
+    end
+  end
+  pigeon_list
 end
